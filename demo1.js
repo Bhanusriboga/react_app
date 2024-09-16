e8352b365dc36e43076ab7af3142f23ae5170512
bhanusri <bhanusri@accumenta.co.in>
2024-09-16 16:39:50 +0530

    newly added files

---
+++ demo1.js
@@ -0,0 +1,5 @@
+let age=20;
+let agestatus=age>=18 ? "adult": "Minor";
+console.log(agestatus);
+
+const accessLevel = userRole === "admin" ? "Full Access" : userRole === "editor" ? "Edit Access" : "View Only";
---
+++ script.js
@@ -0,0 +1,169 @@
+// console.log("Hello k")
+// fullname="hon";
+// console.log(fullname);
+// x="demo";
+// console.log(x);
+
+// let y="1";
+// console.log(y);
+
+// const on ={
+//     fullname:"b",
+//     age:2,
+//     section:"B"
+// };
+// console.log(on)
+
+// const product={
+// name:personalbar,
+// rating:1-5,
+// }
+
+// let a=5;
+// let b=6;
+
+// let cond1=a<b;
+// let cond2=a==b;
+// console.log("cond1 || cond2 =", cond1 || cond2);
+
+// let age=88;
+
+// if(age>=18){
+//     console.log("you can vote");
+    
+// }
+
+// else{
+//     console.log("you cannot vote");
+// }
+
+// let age=20;
+// let result =age >= 777 ? "yes":"no";
+
+// console.log(result);
+
+// let number=prompt ("enter the number");
+
+// if(number%5===0){
+//     console.log("yes")
+// }
+// else{
+//     console.log("no")
+// }
+
+// let score=prompt("enter the number:");
+// let grade;
+// if(score>=80 && score<=100){
+//     console.log("grade is A")
+
+// }
+// else if(score>=70 && score<=89){
+//     console.log("grade is B")
+
+// }
+
+// else if(score>=60 && score<=69){
+//     console.log("grade is C")
+
+// }
+
+// let str="bhanu";
+// for(i of str){
+//     console.log("i=",i);
+// }
+
+// for(let i=0;i<=100;i++)
+// if(i%2===0){
+// console.log("i=",i)
+// }
+// let number=prompt("enter the number:");
+
+// if(number===3){
+//   console.log("yes")
+// }
+// else{x 
+//     console.log("wrong")
+// }
+
+// let gamenum=55;
+// let usernum=prompt("guess it:");
+// while(usernum !=gamenum){
+//     usernum=prompt("you guessed it wrong:");
+// }
+// console.log("you did it");
+
+// let Fullname="Bhanu";
+// let Username= "@"+Fullname+ Fullname.length;
+// console.log(Username)
+
+
+// const x=6; 
+// const y=5; 
+// const z=x-y; 
+// console.log(z);
+ 
+// const a=10;
+// const b=20;
+// const c= a+b;
+// console.log (c);
+
+// const length = 5;  
+// const breadth = 5; 
+// if (length === breadth) {
+//     console.log("It is a square.");
+// } else {
+//     console.log("It is not a square.");
+// }
+
+// let num1 =10 ;
+// let num2 =9 ;
+
+// if (num1 > num2) {
+//     console.log(`The greatest number is: ${num1}`);
+// } else if (num2 > num1) {
+//     console.log(`The greatest number is: ${num2}`);
+// } else {
+//     console.log('Both numbers are equal.');
+// }
+
+// var unitPrice = 100;
+// var discountRate = 0.10;
+// var quantity = 10; 
+// var totalCost = unitPrice * quantity;
+
+// if (totalCost > 1000) {
+//     totalCost = totalCost - (totalCost * discountRate);
+// }
+
+// console.log("The total cost for " + quantity + " units is: " + totalCost);
+
+
+// var number1 = 10;
+// var number2 = 20; 
+// console.log(
+//     number1 > number2 ? "number1 is the largest" : number1 < number2 ? "number2 is the largest" : "number1 and number2 are equal"
+// );
+
+// const num=6;
+// const result = (num % 2 === 0) ? "Num is even" : "Num is odd";
+
+// console.log(result);
+
+let num1 = 15;
+let start = 10;
+let end = 20;
+
+let rangeCheck = (num1 >= start && num1 <= end) ? "Between the range" : "Outside the range";
+
+console.log(rangeCheck);
+
+const number = 45; 
+
+if (number % 2 === 0) {
+  console.log("Number is even");
+} else {
+  console.log("Number is odd");
+}
+
+
+
